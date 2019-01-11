9080a79b8702ae81f68ca5c6c7b456b03758e4a7
julkwel <julienrajerison5@gmail.com>
2019-01-11 10:42:29 +0300

    correction

--- js/boucle.js
+++ js/boucle.js
@@ -1,20 +1,26 @@
 /*Une liste de nombres, de 1 à 55 :*/
 document.write('Une liste de nombres, de 1 à 55 :'+ '<br/>');
-for (var n=1;n<56;n++){
-    document.write(n+' ');
+
+for (var n=1 ; n<56; n++){
+    document.write(n + ' ');
 }
+
 document.writeln('<hr>');
 
 /*nombre impair*/
-document.write('nombre impair de 1 à 55'+'<br/>')
+document.write('nombre impair de 1 à 55'+'<br/>');
+
 for (var nb=1 ; nb<=55 ; nb+=2){
     document.writeln(nb + ' ');
 }
 document.writeln('<hr>');
 
 /*calcul de 30!*/
-document.write('calcul de 30!'+'<br/>')
+document.write('calcul de 30! ' + '<br/>');
 var f=1;
-for( i=1; i<30; i++)
+
+for( i=1; i<30; i++){
     f=f*i;
-    document.writeln('30!=' +f)
+    document.writeln('30!=' +f);
+}
+
